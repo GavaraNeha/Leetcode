e763fba414c00518c739a9dcdce6d7ac219020fb
GavaraNeha <gavaraneha@gmail.com>
2026-09-04 15:30:16 +0530

    Time: 3 ms (18.02%), Space: 50.1 MB (68.57%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -4,8 +4,8 @@ public:
         int count = 0;
         int maxCount = 0;
 
-        for (int num : nums) {
-            if (num == 1) {
+        for (int x : nums) {
+            if (x == 1) {
                 count++;
                 maxCount = max(maxCount, count);
             } else {
